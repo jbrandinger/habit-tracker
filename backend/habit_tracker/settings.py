1bb466c756054dc142836470055aadf7bb7102bc
jbrandinger <98676769+jbrandinger@users.noreply.github.com>
2025-11-15 13:32:39 -0800

    update CORS settings to include port 5173

--- backend/habit_tracker/settings.py
+++ backend/habit_tracker/settings.py
@@ -143,7 +143,7 @@ SIMPLE_JWT = {
 # CORS Settings
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS', 
-    default='http://localhost:3000,http://127.0.0.1:3000'
+    default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
